50b8b306ef7a96023714ce22187b09dac2ae84fc
JohnMasoner <masoner6429@gmail.com>
2022-03-14 15:00:20 +0800

    update dataloader2d to adding the adjacent layer read

--- data/Dataloader2d.py
+++ data/Dataloader2d.py
@@ -12,18 +12,20 @@ class MonoMedDataSets2D(torch.utils.data.Dataset):
     label path: self.file_dir/{file_mode}/label/*.npy
     input path: self.file_dir/{file_mode}/{data_type}/*.npy
     '''
-    def __init__(self, file_dir, transform = None, file_mode = None,data_type=None) -> None:
+    def __init__(self, file_dir, transform = None, file_mode = None,data_type=None, adjacent_layer=None) -> None:
         '''
         Args:
             file_dir: Directory of the file to read
             transform: a transform function to transform
             file_mode: the file mode to read, must be your own root file
             data_type: the data type to read, must be your own data type
+            adjacent_layer: the adjacent layer to read, you could set 2.5d model by the parameter
         '''
         self.file_dir = os.path.join(file_dir, file_mode)
         self.label = sorted(glob.glob(os.path.join(self.file_dir,'*/label/*')))
         self.inputs = sorted(glob.glob(os.path.join(self.file_dir,f'*/{data_type}/*')))
         self.transform = transform
+        self.adjacent_layer = adjacent_layer
 
     def __len__(self) -> int:
         return len(self.label)
@@ -60,7 +62,23 @@ class MonoMedDataSets2D(torch.utils.data.Dataset):
         Return:
             np.ndarray
         '''
+        if self.adjacent_layer is not None:
+            filename_name = filename.split('/')[-1].split('.')[0]
+            filename_dir = os.path.dirname(filename)
+            assert filename_name.isdigit(), 'Filename must be a number'
+            if int(filename_name) >= self.adjacent_layer and int(filename_name) <= len(glob.glob(os.path.join(filename_dir,'*')))-self.adjacent_layer - 1:
+                return self.ReadMultiData(filename_dir, int(filename_name))
+            elif int(filename_name) < self.adjacent_layer:
+                return self.ReadMultiData(filename_dir, int(filename_name) + self.adjacent_layer)
+            else:
+                return self.ReadMultiData(filename_dir, int(filename_name) - self.adjacent_layer -1)
         return np.load(filename).astype(float)
+
+    def ReadMultiData(self, filedir, filename):
+        data = []
+        for i in range(filename-self.adjacent_layer, filename+self.adjacent_layer+1):
+            data.append(np.load(os.path.join(filedir,f'{str(i)}.npy')).astype(float)[np.newaxis,:])
+        return np.vstack(data).astype(float)
 
 class MonoMedDataSets2DTest(MonoMedDataSets2D):
     def __init__(self, file_dir, file_mode = None,data_type=None):
@@ -87,7 +105,7 @@ class MonoMedDataSets2DTest(MonoMedDataSets2D):
         return sample
 
 if __name__ == '__main__':
-    data = MonoMedDataSets2DTest('/raid0/myk/Y064/Dataset', file_mode = 'NPY_train',data_type='CT')
+    data = MonoMedDataSets2D('/raid0/myk/Y064/Dataset', file_mode = 'NPY_train',data_type='CT', adjacent_layer=1)
     data_dl = torch.utils.data.DataLoader(data, 1, False, num_workers= 1)
     for i, sample in enumerate(data_dl):
         print(sample['image'].shape)
