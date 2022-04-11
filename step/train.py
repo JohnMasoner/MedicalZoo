810e6d8c21d416c2261c1725a7a8c2b935fbfdc4
JohnMasoner <masoner6429@gmail.com>
2022-04-11 15:50:19 +0800

    fixed some bugs

--- step/train.py
+++ step/train.py
@@ -15,58 +15,11 @@ from data import Dataloader2d
 from utils.logger import Logger
 from utils.save_data import *
 from metrics.dice import Compute_MeanDice, DiceCoefficient
+from validate.validate import validate
 
-def validate(config, model, epoch, val_dice):
-    '''
-    Validation the model
-    Args:
-        config[ConfigParser]: config parser
-        model[Model]: model to validate
-        epoch[int]: epoch number to be better save model
-        val_dice[int]: dice value to saveing the best model
-    '''
-    dice = 0
-    validate_load = dataset(config, 'test')
-    model.eval()
-    inference = monai.inferers.SlidingWindowInferer(roi_size=320)
-    print('Validation----------------------------------')
-    with torch.no_grad():
-        validate_load = tqdm(validate_load, desc='Validation')
-        for idx, data in enumerate(validate_load):
-            if len(data.keys()) > 2:
-                inputs = MultiLoader(data.keys(), data,'val')
-            else:
-                inputs = data["image"].type(torch.FloatTensor).cuda(non_blocking=True)
-            labels = data["label"].type(torch.FloatTensor).cuda(non_blocking=True)[0]
-            preds = []
-            if config['Data']['Dimension'] == '2':
-                for i in range(inputs.shape[1]):
-                    # outputs = model(inputs[:,i,:])
-                    outputs = inference(inputs[:,i,:], model)
-                    if config['Data']['Save']:
-                        save_data(i, (labels[i,:], outputs))
-                    preds.append(outputs[np.newaxis,:])
-                preds = torch.cat(preds, axis=0)[:,0,:]
-            elif config['Data']['Dimension'] == '3':
-                preds = model(inputs)
-            else:
-                raise ValueError(f'Dimension is not unsupported. It must be 2 or 3')
-            dice += DiceCoefficient(labels, (preds.sigmoid()>0.5).float())
-    print(f'Dice is {dice/len(validate_load)}')
-
-    save_path = os.path.join(config['Paths']['checkpoint_dir'], config['DEFAULT']['Name'], "%s_checkpoint_%04d.pt" % (config['DEFAULT']['Name'], epoch))
-    # store best loss and save a model checkpoint
-    torch.save({"arch": "Mono_2d", "model": model.state_dict()},save_path,)
-    print("Saved checkpoint to: %s" % save_path)
-    if dice/len(validate_load) > val_dice and epoch !=0 :
-        save_path = os.path.join(config['Paths']['checkpoint_dir'], config['DEFAULT']['Name'], f"{config['DEFAULT']['Name']}_checkpoint_best_model.pt")
-        torch.save({"acc":dice/len(validate_load),"arch": "Mono_2d", "model": model.state_dict()},save_path,)
-        return dice/len(validate_load)
-    else:
-        return val_dice
 
 def trainer(config):
-    model = network(config).train(); val_dice = 0.0
+    model = network(config); val_dice = 0.0
     if config['Model']['LoadModel']:
         checkpoint = torch.load(config['Model']['LoadModel'])
         model.load_state_dict(checkpoint["model"])
@@ -88,6 +41,7 @@ def trainer(config):
         print("Epoch {}/{}".format(epoch+1, config['Training']['MaxEpoch']))
         print("-" * 10)
         # iterate all data
+        model.train()
         for idx, data in enumerate(tqdm(train_dataload, desc="training")):
             if len(data.keys()) > 2:
                 inputs = MultiLoader(data.keys(), data)
