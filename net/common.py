4e0e8bf0aac21079254bada7624047f006efc2a5
Mason Ma <masoner6429@gmail.com>
2023-04-26 16:07:38 +0800

    remake
    Signed-off-by: Mason Ma <masoner6429@gmail.com>

--- net/common.py
+++
@@ -1,8 +0,0 @@
-import monai
-
-def network(config):
-    net = config['Model']['Model'].lower()
-    if net == 'unet':
-        return monai.networks.nets.BasicUNet(spatial_dims=2, in_channels=int(config['Model']['in_channels']), out_channels=int(config['Model']['out_channels'])).cuda()
-    else:
-        raise Exception('Unknown network type')
--- test_code.py
+++
@@ -1,6 +0,0 @@
-from transforms import common
-import configparser
-config = configparser.ConfigParser()
-config.read('config/sample.ini')
-a = common.transform(config)
-print(a)
